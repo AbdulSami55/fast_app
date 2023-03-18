f7883981a2882d26c6afa779c2dd20cfe19b7f6d
saqib-258 <saqib.syed258@gmail.com>
2023-03-18 10:53:18 +0500

    login_signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:fast_app/Screens/Admin/home.dart';
 import 'package:fast_app/bindings.dart';
+import 'package:fast_app/screens/Login/login_screen.dart';
+import 'package:fast_app/screens/Login/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
@@ -13,9 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       initialBinding: ControllerBindings(),
-      title: 'Flutter Demo',
-      home: const Home(),
+      home: SignupScreen(),
     );
   }
 }
